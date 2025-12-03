b81ef3bbb677431e1f32cf7485a24c7e3546a58a
Andrew Davis <1709934+Savid@users.noreply.github.com>
2025-12-03 13:54:59 +1000

    feat: update simple processor to match execution_transaction schema
    Update Transaction struct and processing logic to output data matching
    the ClickHouse execution_transaction table schema:
    
    - Rename fields to match schema (position, hash, from, to, gas, type, etc.)
    - Add effective gas price calculation for EIP-1559+ transactions
    - Add blob transaction fields (blob_gas, blob_gas_fee_cap, blob_hashes)
    - Add transaction size and call_data_size fields
    - Use string representation for UInt128 fields to avoid precision loss
    - Remove fields not in schema (input, gas_used)

--- pkg/processor/transaction/simple/handlers.go
+++ pkg/processor/transaction/simple/handlers.go
@@ -2,7 +2,6 @@ package simple
 
 import (
 	"context"
-	"encoding/hex"
 	"fmt"
 	"math/big"
 	"time"
@@ -16,33 +15,36 @@ import (
 	"github.com/ethpandaops/execution-processor/pkg/processor/transaction/structlog"
 )
 
-// Transaction represents a row in the transaction table.
+// Transaction represents a row in the execution_transaction table.
 //
 //nolint:tagliatelle // ClickHouse uses snake_case column names
 type Transaction struct {
-	UpdatedDateTime      structlog.ClickHouseTime `json:"updated_date_time"`
-	BlockNumber          uint64                   `json:"block_number"`
-	BlockHash            string                   `json:"block_hash"`
-	ParentHash           string                   `json:"parent_hash"`
-	TransactionIndex     uint64                   `json:"transaction_index"`
-	TransactionHash      string                   `json:"transaction_hash"`
-	Nonce                uint64                   `json:"nonce"`
-	FromAddress          string                   `json:"from_address"`
-	ToAddress            *string                  `json:"to_address"`
-	Value                *big.Int                 `json:"value"` // Wei as UInt256
-	Input                *string                  `json:"input"` // Hex-encoded input data
-	GasLimit             uint64                   `json:"gas_limit"`
-	GasUsed              uint64                   `json:"gas_used"`
-	GasPrice             uint64                   `json:"gas_price"`
-	TransactionType      uint32                   `json:"transaction_type"`
-	MaxPriorityFeePerGas uint64                   `json:"max_priority_fee_per_gas"`
-	MaxFeePerGas         uint64                   `json:"max_fee_per_gas"`
-	Success              bool                     `json:"success"`
-	NInputBytes          uint32                   `json:"n_input_bytes"`
-	NInputZeroBytes      uint32                   `json:"n_input_zero_bytes"`
-	NInputNonzeroBytes   uint32                   `json:"n_input_nonzero_bytes"`
-	MetaNetworkID        int32                    `json:"meta_network_id"`
-	MetaNetworkName      string                   `json:"meta_network_name"`
+	UpdatedDateTime    structlog.ClickHouseTime `json:"updated_date_time"`
+	BlockNumber        uint64                   `json:"block_number"`
+	BlockHash          string                   `json:"block_hash"`
+	ParentHash         string                   `json:"parent_hash"`
+	Position           uint32                   `json:"position"`
+	Hash               string                   `json:"hash"`
+	From               string                   `json:"from"`
+	To                 *string                  `json:"to"`
+	Nonce              uint64                   `json:"nonce"`
+	GasPrice           string                   `json:"gas_price"`        // Effective gas price as UInt128 string
+	Gas                uint64                   `json:"gas"`              // Gas limit
+	GasTipCap          *string                  `json:"gas_tip_cap"`      // Nullable UInt128 string
+	GasFeeCap          *string                  `json:"gas_fee_cap"`      // Nullable UInt128 string
+	Value              string                   `json:"value"`            // UInt128 string
+	Type               uint8                    `json:"type"`             // Transaction type
+	Size               uint32                   `json:"size"`             // Transaction size in bytes
+	CallDataSize       uint32                   `json:"call_data_size"`   // Size of call data
+	BlobGas            *uint64                  `json:"blob_gas"`         // Nullable - for type 3 txs
+	BlobGasFeeCap      *string                  `json:"blob_gas_fee_cap"` // Nullable UInt128 string
+	BlobHashes         []string                 `json:"blob_hashes"`      // Array of versioned hashes
+	Success            bool                     `json:"success"`
+	NInputBytes        uint32                   `json:"n_input_bytes"`
+	NInputZeroBytes    uint32                   `json:"n_input_zero_bytes"`
+	NInputNonzeroBytes uint32                   `json:"n_input_nonzero_bytes"`
+	MetaNetworkID      int32                    `json:"meta_network_id"`
+	MetaNetworkName    string                   `json:"meta_network_name"`
 }
 
 // GetHandlers returns the task handlers for this processor.
@@ -231,17 +233,9 @@ func (p *Processor) buildTransactionRow(
 		toAddress = &addr
 	}
 
-	// Build input data
-	var input *string
-
-	inputData := tx.Data()
-	if len(inputData) > 0 {
-		inputHex := "0x" + hex.EncodeToString(inputData)
-		input = &inputHex
-	}
-
 	// Calculate input byte stats
-	nInputBytes := uint32(len(inputData)) //nolint:gosec // data length is bounded
+	inputData := tx.Data()
+	callDataSize := uint32(len(inputData)) //nolint:gosec // data length is bounded
 
 	var nInputZeroBytes, nInputNonzeroBytes uint32
 
@@ -253,48 +247,116 @@ func (p *Processor) buildTransactionRow(
 		}
 	}
 
-	// Get gas price (for legacy transactions)
-	var gasPrice uint64
-	if tx.GasPrice() != nil {
-		gasPrice = tx.GasPrice().Uint64()
+	// Calculate effective gas price
+	gasPrice := calculateEffectiveGasPrice(block, tx)
+
+	// Get EIP-1559 fee fields (nullable for legacy transactions)
+	var gasTipCap, gasFeeCap *string
+
+	if tx.Type() >= 2 { // EIP-1559 and later
+		if tx.GasTipCap() != nil {
+			tipCap := tx.GasTipCap().String()
+			gasTipCap = &tipCap
+		}
+
+		if tx.GasFeeCap() != nil {
+			feeCap := tx.GasFeeCap().String()
+			gasFeeCap = &feeCap
+		}
 	}
 
-	// Get EIP-1559 fee fields
-	var maxPriorityFeePerGas, maxFeePerGas uint64
-
-	if tx.GasTipCap() != nil {
-		maxPriorityFeePerGas = tx.GasTipCap().Uint64()
+	// Get transaction value
+	value := "0"
+	if tx.Value() != nil {
+		value = tx.Value().String()
 	}
 
-	if tx.GasFeeCap() != nil {
-		maxFeePerGas = tx.GasFeeCap().Uint64()
+	// Get transaction size
+	txSize := uint32(tx.Size()) //nolint:gosec // tx size is bounded
+
+	// Build transaction row
+	txRow := Transaction{
+		UpdatedDateTime:    structlog.NewClickHouseTime(time.Now()),
+		BlockNumber:        block.Number().Uint64(),
+		BlockHash:          block.Hash().String(),
+		ParentHash:         block.ParentHash().String(),
+		Position:           uint32(index), //nolint:gosec // index is bounded by block size
+		Hash:               tx.Hash().String(),
+		From:               from.Hex(),
+		To:                 toAddress,
+		Nonce:              tx.Nonce(),
+		GasPrice:           gasPrice.String(),
+		Gas:                tx.Gas(),
+		GasTipCap:          gasTipCap,
+		GasFeeCap:          gasFeeCap,
+		Value:              value,
+		Type:               tx.Type(),
+		Size:               txSize,
+		CallDataSize:       callDataSize,
+		BlobHashes:         []string{}, // Default empty array
+		Success:            receipt.Status == 1,
+		NInputBytes:        callDataSize,
+		NInputZeroBytes:    nInputZeroBytes,
+		NInputNonzeroBytes: nInputNonzeroBytes,
+		MetaNetworkID:      p.network.ID,
+		MetaNetworkName:    p.network.Name,
 	}
 
-	return Transaction{
-		UpdatedDateTime:      structlog.NewClickHouseTime(time.Now()),
-		BlockNumber:          block.Number().Uint64(),
-		BlockHash:            block.Hash().String(),
-		ParentHash:           block.ParentHash().String(),
-		TransactionIndex:     index,
-		TransactionHash:      tx.Hash().String(),
-		Nonce:                tx.Nonce(),
-		FromAddress:          from.Hex(),
-		ToAddress:            toAddress,
-		Value:                tx.Value(),
-		Input:                input,
-		GasLimit:             tx.Gas(),
-		GasUsed:              receipt.GasUsed,
-		GasPrice:             gasPrice,
-		TransactionType:      uint32(tx.Type()),
-		MaxPriorityFeePerGas: maxPriorityFeePerGas,
-		MaxFeePerGas:         maxFeePerGas,
-		Success:              receipt.Status == 1,
-		NInputBytes:          nInputBytes,
-		NInputZeroBytes:      nInputZeroBytes,
-		NInputNonzeroBytes:   nInputNonzeroBytes,
-		MetaNetworkID:        p.network.ID,
-		MetaNetworkName:      p.network.Name,
-	}, nil
+	// Handle blob transaction fields (type 3)
+	if tx.Type() == types.BlobTxType {
+		blobGas := tx.BlobGas()
+		txRow.BlobGas = &blobGas
+
+		if tx.BlobGasFeeCap() != nil {
+			blobGasFeeCap := tx.BlobGasFeeCap().String()
+			txRow.BlobGasFeeCap = &blobGasFeeCap
+		}
+
+		// Get versioned hashes
+		blobHashes := tx.BlobHashes()
+		txRow.BlobHashes = make([]string, len(blobHashes))
+
+		for i, hash := range blobHashes {
+			txRow.BlobHashes[i] = hash.String()
+		}
+	}
+
+	return txRow, nil
+}
+
+// calculateEffectiveGasPrice calculates the effective gas price for a transaction.
+// For legacy/access list txs: returns tx.GasPrice().
+// For EIP-1559+ txs: returns min(max_fee_per_gas, base_fee + max_priority_fee_per_gas).
+func calculateEffectiveGasPrice(block *types.Block, tx *types.Transaction) *big.Int {
+	txType := tx.Type()
+
+	// Legacy and access list transactions use GasPrice directly
+	if txType == types.LegacyTxType || txType == types.AccessListTxType {
+		if tx.GasPrice() != nil {
+			return tx.GasPrice()
+		}
+
+		return big.NewInt(0)
+	}
+
+	// EIP-1559, blob, and 7702 transactions need effective gas price calculation
+	baseFee := block.BaseFee()
+	if baseFee == nil {
+		// Fallback for pre-EIP-1559 blocks (shouldn't happen for type 2+ txs)
+		if tx.GasPrice() != nil {
+			return tx.GasPrice()
+		}
+
+		return big.NewInt(0)
+	}
+
+	// Effective gas price = min(max_fee_per_gas, base_fee + max_priority_fee_per_gas)
+	effectiveGasPrice := new(big.Int).Add(baseFee, tx.GasTipCap())
+	if effectiveGasPrice.Cmp(tx.GasFeeCap()) > 0 {
+		effectiveGasPrice = tx.GasFeeCap()
+	}
+
+	return effectiveGasPrice
 }
 
 // insertTransactions inserts transactions into ClickHouse.
